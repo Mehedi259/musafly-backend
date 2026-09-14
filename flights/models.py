e75c5f02c0917d46b5e15e3e3d62e8f953d44df6
mehedi259 <mmridul207@gmail.com>
2026-09-15 01:23:52 +0600

    Add image field to Flight and Visa models

--- flights/models.py
+++ flights/models.py
@@ -7,6 +7,7 @@ class Flight(models.Model):
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="flights/", blank=True, null=True)
 
     def __str__(self):
         return f"{self.airline}: {self.origin} to {self.destination}"
--- visas/models.py
+++ visas/models.py
@@ -6,6 +6,7 @@ class Visa(models.Model):
     processing_time = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     requirements = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="visas/", blank=True, null=True)
 
     def __str__(self):
         return f"{self.country} - {self.visa_type}"

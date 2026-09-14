237ec1cb2c0f662b7380ffb2e13e0da00d498e5d
mehedi259 <mmridul207@gmail.com>
2026-09-15 01:10:56 +0600

    Add image field to UmrahPackage

--- umrah/models.py
+++ umrah/models.py
@@ -4,6 +4,7 @@ class UmrahPackage(models.Model):
     package_name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inclusions = models.TextField()
+    image = models.ImageField(upload_to="umrah/", blank=True, null=True)
 
     def __str__(self):
         return self.package_name
